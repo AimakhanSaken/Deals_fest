1365e5dc7a48d580a1880719e15aac0d60ed1376
AimakhanSaken <99489326+AimakhanSaken@users.noreply.github.com>
2022-04-09 20:23:01 +0600

    Deals

---
+++ js/script.js
@@ -0,0 +1,198 @@
+var countDownDate = new Date("Jun 10, 2022 16:37:52").getTime();
+
+// Run myfunc every second
+var myfunc = setInterval(function() {
+
+var now = new Date().getTime();
+var timeleft = countDownDate - now;
+    
+// Calculating the days, hours, minutes and seconds left
+var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+// Result is output to the specific element
+document.getElementById("days").innerHTML = days 
+document.getElementById("hours").innerHTML = hours  
+document.getElementById("minutes").innerHTML = minutes  
+document.getElementById("seconds").innerHTML = seconds 
+    
+// Display the message when countdown is over
+if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("days").innerHTML = ""
+    document.getElementById("hours").innerHTML = "" 
+    document.getElementById("minutes").innerHTML = ""
+    document.getElementById("seconds").innerHTML = ""
+    document.getElementById("end").innerHTML = "TIME UP!!";
+}
+}, 1000);
+//---------------------------------------------------------------------------------------------------//
+var countDownDate = new Date("Jun 10, 2022 16:37:52").getTime();
+
+// Run myfunc every second
+var myfunc = setInterval(function() {
+
+var now = new Date().getTime();
+var timeleft = countDownDate - now;
+    
+// Calculating the days, hours, minutes and seconds left
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+// Result is output to the specific element
+document.getElementById("hrs").innerHTML = hours  
+document.getElementById("mins").innerHTML = minutes  
+document.getElementById("secs").innerHTML = seconds 
+    
+// Display the message when countdown is over
+if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("hrs").innerHTML = "" 
+    document.getElementById("mins").innerHTML = ""
+    document.getElementById("secs").innerHTML = ""
+    document.getElementById("end").innerHTML = "TIME UP!!";
+}
+}, 1000);
+//---------------------------------------------------------------------------------------------------------//
+var countDownDate = new Date("Jun 10, 2022 16:37:52").getTime();
+
+// Run myfunc every second
+var myfunc = setInterval(function() {
+
+var now = new Date().getTime();
+var timeleft = countDownDate - now;
+    
+// Calculating the days, hours, minutes and seconds left
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+// Result is output to the specific element
+document.getElementById("hr").innerHTML = hours  
+document.getElementById("min").innerHTML = minutes  
+document.getElementById("sec").innerHTML = seconds 
+    
+// Display the message when countdown is over
+if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("hr").innerHTML = "" 
+    document.getElementById("min").innerHTML = ""
+    document.getElementById("sec").innerHTML = ""
+    document.getElementById("end").innerHTML = "TIME UP!!";
+}
+}, 1000);
+//---------------------------------------------------------------------------------------------------//
+var countDownDate = new Date("Jun 10, 2022 16:37:52").getTime();
+
+// Run myfunc every second
+var myfunc = setInterval(function() {
+
+var now = new Date().getTime();
+var timeleft = countDownDate - now;
+    
+// Calculating the days, hours, minutes and seconds left
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+// Result is output to the specific element
+document.getElementById("h").innerHTML = hours  
+document.getElementById("m").innerHTML = minutes  
+document.getElementById("s").innerHTML = seconds 
+    
+// Display the message when countdown is over
+if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("h").innerHTML = "" 
+    document.getElementById("m").innerHTML = ""
+    document.getElementById("s").innerHTML = ""
+    document.getElementById("end").innerHTML = "TIME UP!!";
+}
+}, 1000);
+//---------------------------------------------------------------------------------------------------//
+var countDownDate = new Date("Jun 10, 2022 16:37:52").getTime();
+
+// Run myfunc every second
+var myfunc = setInterval(function() {
+
+var now = new Date().getTime();
+var timeleft = countDownDate - now;
+    
+// Calculating the days, hours, minutes and seconds left
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+// Result is output to the specific element
+document.getElementById("chasy").innerHTML = hours  
+document.getElementById("minyty").innerHTML = minutes  
+document.getElementById("secyndy").innerHTML = seconds 
+    
+// Display the message when countdown is over
+if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("chasy").innerHTML = "" 
+    document.getElementById("minyty").innerHTML = ""
+    document.getElementById("secyndy").innerHTML = ""
+    document.getElementById("end").innerHTML = "TIME UP!!";
+}
+}, 1000);
+//--------------------------------------------------------------------------------------------------//
+var countDownDate = new Date("Jun 10, 2022 16:37:52").getTime();
+
+// Run myfunc every second
+var myfunc = setInterval(function() {
+
+var now = new Date().getTime();
+var timeleft = countDownDate - now;
+    
+// Calculating the days, hours, minutes and seconds left
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+// Result is output to the specific element
+document.getElementById("chas").innerHTML = hours  
+document.getElementById("minyt").innerHTML = minutes  
+document.getElementById("secynd").innerHTML = seconds 
+    
+// Display the message when countdown is over
+if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("chas").innerHTML = "" 
+    document.getElementById("minyt").innerHTML = ""
+    document.getElementById("secynd").innerHTML = ""
+    document.getElementById("end").innerHTML = "TIME UP!!";
+}
+}, 1000);
+//---------------------------------------------------------------------------------------------------//
+var countDownDate = new Date("Jun 10, 2022 16:37:52").getTime();
+
+// Run myfunc every second
+var myfunc = setInterval(function() {
+
+var now = new Date().getTime();
+var timeleft = countDownDate - now;
+    
+// Calculating the days, hours, minutes and seconds left
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    
+// Result is output to the specific element
+document.getElementById("ch").innerHTML = hours  
+document.getElementById("miny").innerHTML = minutes  
+document.getElementById("secyn").innerHTML = seconds 
+    
+// Display the message when countdown is over
+if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("ch").innerHTML = "" 
+    document.getElementById("miny").innerHTML = ""
+    document.getElementById("secyn").innerHTML = ""
+    document.getElementById("end").innerHTML = "TIME UP!!";
+}
+}, 1000);
